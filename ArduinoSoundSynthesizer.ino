4f627cc179bc72fdc8d8c18ebec95a27148b57bc
Solar138 <46548002+solar138@users.noreply.github.com>
2021-03-08 08:52:33 -0800

    Added record functionality to ArduinoSoundSynthesizer.ino
    Added record functionality to pin 11 and plays tune when first starting up.

--- ArduinoSoundSynthesizer.ino
+++ ArduinoSoundSynthesizer.ino
@@ -6,44 +6,48 @@ const int PIN_ADD = 13;
 float frequencies[48] = {130.82, 146.82, 164.81, 174.61, 196, 220, 246.94, 261.63, 293.66, 329.63, 349.23, 392, 440, 493.88, 523.25, 587.33, 659.26, 698.46, 784, 880, 987.77, 1046.5, 1174.66, 1318.51, 1396.91, 1568, 1760, 1975.53, 2093};
 float frequencyMultiplier = 1;
 float frequencyOffset = 0;
-float interval = 0.1;
-float dutyCycle = 0.25;
+float interval = 0.05;
+float dutyCycle = 0.5;
 
-byte powersOfTwo[8] = {1, 2, 4, 8, 16, 32, 64, 128};
-
-int transpose = 7;
-int songLength = 9;
-int song[] = {0, 1, 2, 3, 4, 5, 6, 7, -100};
+byte transpose = 7;
+int songLength = 8;
 int songIndex = 0;
-int pins[8] = {3, 4, 5, 6, 7, 8, 9, 10};
+byte song[1024] = {0, 1, 2, 3, 4, 5, 6, 7, -100};
+byte pins[8] = {3, 4, 5, 6, 7, 8, 9, 10};
 
 bool songMode = true;
 bool recording = false;
+bool precording = false;
 
 void setup() {
   pinMode(PIN, OUTPUT);
+
+  Serial.begin(9600);
 
   for (int i = 0; i < 8; i++) {
     pinMode(pins[i], INPUT);
     digitalWrite(pins[i], HIGH); // turn on pullup resistors
   }
-  Serial.begin(9600);
+
+  digitalWrite(PIN_RECORD, HIGH);
+  digitalWrite(PIN_SUB, HIGH);
+  digitalWrite(PIN_ADD, HIGH);
+  
   //for (int i = 0; i < 48; i++) {
   //  frequencies[i] = 440 * powf(2, i / 12);
   //  Serial.println(frequencies[i]);
   //}
 
-  Serial.end();
 }
 
 void loop() {  
   bool played = true;
   if (songMode && song[songIndex] == -100) {
-    delay(400);
+    delay(100);
   }
   if (digitalRead(PIN_SUB) == LOW && digitalRead(PIN_ADD) == LOW) {
     songMode = true;
-    interval = 0.4;
+    interval = 0.1;
     songIndex = 0;
   }
   
@@ -60,21 +64,43 @@ void loop() {
         digitalWrite(PIN, false);
         delayMicroseconds (d2);
       }
+      if (recording) {
+        Serial.println(i);
+        song[songLength] = i;
+        songLength++;
+      }
       played = false;
     }
   }
 
-  if (digitalRead(PIN_RECORD)) {
-    recording = true;
+  if (played && recording) {
+    song[songLength] = -100;
+    songLength++;
   }
-  if (songMode) {
+  if (played) {
     delay(100);
+  }
+
+  if (digitalRead(PIN_RECORD) == LOW) {
+    if (!precording) {
+      precording = true;
+      recording = !recording;
+      if (recording) {
+        songLength = 0;
+      }
+    }
+  } else {
+    precording = false;
+  }
+
+  if (songMode) {
+    if (interval == 0.05) {
+      delay(50);
+    }
     songIndex++;
     if (songIndex > songLength) {
-      delay (500);
-      songMode = false;
       interval = 0.1;
+      songMode = false;
     }
   }
 }
-
