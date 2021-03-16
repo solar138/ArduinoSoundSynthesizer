460cb681224fdfdac77d795e5d87f3b125b3bd4c
Solar138 <46548002+solar138@users.noreply.github.com>
2021-03-16 16:27:53 -0700

    Added saving to EEPROM and serial print lines

--- ArduinoSoundSynthesizer.ino
+++ ArduinoSoundSynthesizer.ino
@@ -1,3 +1,5 @@
+#include <EEPROM.h>
+
 const int PIN = 2;
 const int PIN_RECORD = 11;
 const int PIN_SUB = 12;
@@ -6,24 +8,27 @@ const int PIN_ADD = 13;
 float frequencies[48] = {130.82, 146.82, 164.81, 174.61, 196, 220, 246.94, 261.63, 293.66, 329.63, 349.23, 392, 440, 493.88, 523.25, 587.33, 659.26, 698.46, 784, 880, 987.77, 1046.5, 1174.66, 1318.51, 1396.91, 1568, 1760, 1975.53, 2093};
 float frequencyMultiplier = 1;
 float frequencyOffset = 0;
-float interval = 0.05;
+float interval = 0.1;
 float dutyCycle = 0.5;
 
 byte transpose = 7;
-int songLength = 8;
+int songLength = 512;
 int songIndex = 0;
-byte song[1024] = {0, 1, 2, 3, 4, 5, 6, 7, -100};
+byte song[512] = {0, 1, 2, 3, 4, 5, 6, 7, -100};
 byte pins[8] = {3, 4, 5, 6, 7, 8, 9, 10};
+byte clearIndex = 0;
 
 bool songMode = true;
 bool recording = false;
 bool precording = false;
 
 void setup() {
-  pinMode(PIN, OUTPUT);
 
   Serial.begin(9600);
+  Serial.println(F("Initializing PINs..."));
 
+  pinMode(PIN, OUTPUT);
+  
   for (int i = 0; i < 8; i++) {
     pinMode(pins[i], INPUT);
     digitalWrite(pins[i], HIGH); // turn on pullup resistors
@@ -33,11 +38,24 @@ void setup() {
   digitalWrite(PIN_SUB, HIGH);
   digitalWrite(PIN_ADD, HIGH);
   
+  Serial.println(F("PIN Initialization complete"));
+  
   //for (int i = 0; i < 48; i++) {
   //  frequencies[i] = 440 * powf(2, i / 12);
   //  Serial.println(frequencies[i]);
   //}
 
+  Serial.println(F("Loading song from EEPROM..."));
+  EEPROM.get(0, song);
+  for (int i = 0; i < 512; i++) {
+    if (song[i] == 255) {
+      songIndex = i;
+      break;
+    }
+    Serial.println(song[i]);      
+  }
+  Serial.println(F("Loading complete"));
+  Serial.println(F("Press any button to play"));
 }
 
 void loop() {  
@@ -50,6 +68,7 @@ void loop() {
     interval = 0.1;
     songIndex = 0;
   }
+
   
   for (int i = 0; i < 8; i++) {
     if (digitalRead(pins[i]) == LOW || (songMode && i == song[songIndex])) {
@@ -64,8 +83,8 @@ void loop() {
         digitalWrite(PIN, false);
         delayMicroseconds (d2);
       }
-      if (recording) {
         Serial.println(i);
+      if (recording) {
         song[songLength] = i;
         songLength++;
       }
@@ -81,12 +100,39 @@ void loop() {
     delay(100);
   }
 
+  if (digitalRead(PIN_RECORD) == LOW && digitalRead(PIN_SUB) == LOW) {
+    delay(100);
+    clearIndex++;
+    if (clearIndex == 3) {
+      Serial.println(F("Holding down both PIN_RECORD and PIN_SUB at the same time, Hold for 3 seconds to clear EEPROM"));
+    }
+    if (clearIndex == 31) {
+      Serial.println(F("Clearing EEPROM..."));
+
+      for (int i = 0 ; i < EEPROM.length() ; i++) {
+        EEPROM.write(i, 255);
+      }
+      clearIndex = 0;
+
+      songMode = false;
+      
+      Serial.println(F("EEPROM sucessfully cleared"));
+    }
+  } else {
+    clearIndex = 0;
+  }
+
   if (digitalRead(PIN_RECORD) == LOW) {
     if (!precording) {
+      Serial.println(F("Beginning song record..."));
       precording = true;
       recording = !recording;
       if (recording) {
         songLength = 0;
+      } else {
+        Serial.println(F("Saving song to EEPROM..."));
+        EEPROM.put(0, song);
+        Serial.println(F("Save Sucessful."));
       }
     }
   } else {
@@ -100,6 +146,10 @@ void loop() {
     songIndex++;
     if (songIndex > songLength) {
       interval = 0.1;
+      songMode = false;
+      Serial.println(F("Playback Complete.."));
+    }
+    if (song[songIndex] == 255) {
       songMode = false;
     }
   }
